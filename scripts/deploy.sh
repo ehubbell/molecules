180f7ac835085fefc4618348c8d03b9435199e60
playbooks <eric@erichubbell.com>
2025-06-16 15:36:39 -0600

    publish

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then
 fi
 
 if [ -z "$2" ]; then
-  echo -e " \n please include a version. \n"
+  echo -e " \nplease include a version.\m"
   exit
 fi
 
@@ -35,6 +35,19 @@ if [ $? -eq 1 ]; then exit; fi
 echo -e "\n git push \n"
 git push & push_id=$!
 wait $push_id
+if [ $? -eq 1 ]; then exit; fi
+
+echo -e "\n npm version \n"
+npm version $2
+
+echo -e "\n npm build \n"
+npm run build & build_id=$!
+wait $build_id
+if [ $? -eq 1 ]; then exit; fi
+
+echo -e "\n npm publish \n"
+npm publish & publish_id=$!
+wait $publish_id
 if [ $? -eq 1 ]; then exit; fi
 
 echo -e "\n git push tags \n"
