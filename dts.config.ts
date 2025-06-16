e6f929c619c97a14748fedaa6202d3f467031590
playbooks <eric@erichubbell.com>
2025-06-16 15:51:14 -0600

    molecule updates

--- dts.config.ts
+++ dts.config.ts
@@ -1,10 +1,11 @@
 const fileNames = ['index', 'toasts'];
 
 const config = {
+	compilationOptions: { preferredConfigPath: './tsconfig.json' },
 	entries: fileNames.map(fileName => {
 		if (fileName !== 'index') {
 			return {
-				filePath: `./src/molecules/${fileName}/${fileName}.ts`,
+				filePath: `./src/molecules/${fileName}/index.tsx`,
 				outFile: `./dist/${fileName}.d.ts`,
 				noCheck: false,
 			};
